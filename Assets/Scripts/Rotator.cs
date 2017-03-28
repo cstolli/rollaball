01872cc4260b41e3c482fc7924fc8b71e62df0a8
Chris Stoll <chrisstoll@Chriss-MacBook-Pro.local>
2017-03-27 20:13:38 -0700

    first commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	public float speed = 1;
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime * speed);
+	}
+}
